55e4267672461ab5a3e42072f459124a84656949
DivyarajSinh04 <202201155@daiict.ac.in>
2024-11-14 00:58:56 +0530

    Created hotel Schema and hotel route

---
+++ hotel_booking_app/src/Backend-2/models/booking.model.js
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const bookingSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    hotelId: {
+        type: Schema.Types.ObjectId,
+        ref: "HotelDetails",
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    roomCount: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    checkIn: {
+        type: Date,
+        required: true
+    },
+    checkOut: {
+        type: Date,
+        required: true
+    },
+    totalCost: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export const BookingDetails = mongoose.model("BookingDetails", bookingSchema);
