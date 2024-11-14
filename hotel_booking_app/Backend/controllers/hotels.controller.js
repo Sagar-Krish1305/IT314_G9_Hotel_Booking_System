149432d9896da6afd50c00607aab2a8ac70ea3cc
Tathya-Prajapati <202201170@daiict.ac.in>
2024-11-14 14:19:19 +0530

    Create Rating Schema and Search Controller

--- hotel_booking_app/Backend/controllers/hotels.controller.js
+++ hotel_booking_app/Backend/controllers/hotels.controller.js
@@ -5,6 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import mongoose from "mongoose";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { HotelDetails } from "../models/hotel.model.js";
+import { BookingDetails } from "../models/booking.model.js";
 
 
 const RegisterHotel = asyncHandler(async(req, res)=>{
@@ -70,6 +71,70 @@ const RegisterHotel = asyncHandler(async(req, res)=>{
 })
 
 
+
+
+const handleSearchRequest = asyncHandler(async(req,res)=>{
+    // data : {city, checkindate,checkoutdate, no of required rooms}
+    // retrive all hotels - in the city, with available no of rooms during booking period
+    //    --> retrive hotels which are in the city
+    //    --> for each hotel look in bookingDetails and retrive no of booking of that hotel during booking period 
+    //    --> store all hotels for which available room is more than required rooms
+
+    const {city, checkInDate, checkOutDate, requiredRooms} = req.body
+    // console.log("123");
+    // console.log(req.body);
+    // console.log(city, checkInDate, checkOutDate, requiredRooms);
+    if(!(city && checkInDate && checkOutDate && requiredRooms)){
+        throw new ApiError(400, "all fields are required")
+    }
+
+    // Find hotels in the specified city
+    const hotelsInCity = await HotelDetails.find({ city });
+
+    const availableHotels = [];
+
+    // Iterate over each hotel and check room availability
+    for (const hotel of hotelsInCity) {
+        const bookedRooms = await BookingDetails.aggregate([
+            {
+                $match: {
+                    hotelId: hotel._id,
+
+                    $and: [
+                        { checkIn: { $lte: new Date(checkOutDate) } },
+                        { checkOut: { $gte: new Date(checkInDate) } }
+                    ]
+                }
+            },
+            {
+                $group: {
+                    _id: "$hotelId",
+                    totalRoomsBooked: { $sum: "$roomCount" }
+                }
+            }
+        ]);
+
+        const totalRoomsBooked = bookedRooms.length > 0 ? bookedRooms[0].totalRoomsBooked : 0;
+        const availableRooms = hotel.roomCount - totalRoomsBooked;
+
+        // Check if the available rooms meet the required number of rooms
+        if (availableRooms >= requiredRooms) {
+            const { roomCount, password, ...hotelData } = hotel.toObject();
+            availableHotels.push({...hotelData, availableRooms});
+        }
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, availableHotels, "All hotels with required search condition is returned")
+    )
+
+})
+
+
+
 export {
-    RegisterHotel
+    RegisterHotel,
+    handleSearchRequest
 }
---
+++ hotel_booking_app/Backend/models/rating.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+const ratingSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    overallRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    reviewTitle: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reviewDescription: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    serviceRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    roomsRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    cleanlinessRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    foodRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    reviewImages: {
+        type: [String],
+        defaule: []
+    }
+}, { _id: false }, 
+   {timestamps : true});
+
+export const Rating = mongoose.model("Rating", ratingSchema);
