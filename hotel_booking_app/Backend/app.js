db8a45503b480268d66494cdd6f1251535420b16
202201183 <malharvahasiya1811@gmail.com>
2024-12-01 11:58:00 +0530

    About Us page and adding link of frontend in backend

--- hotel_booking_app/Backend/app.js
+++ hotel_booking_app/Backend/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',  // Specify the origin
+    origin: 'https://stayeazy.vercel.app/home',  // Specify the origin
     credentials: true                // Allow credentials
   }));
 
